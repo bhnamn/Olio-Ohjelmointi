d178030df9733d4b0f255ebed5f0ec1c0dcf8663
bhnamn <borhan.amini@gmail.com>
2016-01-15 00:26:26 +0200

    initial try

---
+++ Demo1_Teh11/Demo1_Teh11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_Teh11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row;
+            Console.Write("Enter row number: ");
+            row = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < row; i++)
+            {
+
+            }
+
+        }
+    }
+}
