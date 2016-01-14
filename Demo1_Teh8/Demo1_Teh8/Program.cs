7c305572edd74d0a8a6b6274c64ef64fdb1cec2a
bhnamn <borhan.amini@gmail.com>
2016-01-14 23:36:30 +0200

    Demo1_Teh8_The best solution :)

--- Demo1_Teh8/Demo1_Teh8/Program.cs
+++ Demo1_Teh8/Demo1_Teh8/Program.cs
@@ -12,25 +12,28 @@ namespace Demo1_Teh8
         {
             int a, b, c, x, y, z;
             Console.WriteLine("Please enter 3 integer numbers: ");
-            a = int.Parse(Console.ReadLine());
-            b = int.Parse(Console.ReadLine());
-            c = int.Parse(Console.ReadLine());
-           
+            a = x = int.Parse(Console.ReadLine());
+            b = y = int.Parse(Console.ReadLine());
+            c = z = int.Parse(Console.ReadLine());
+
             if (b > a)
             {
-                a = b; 
+                a = y;
+                b = x;
             }
-            //Console.WriteLine(a + "," + b + "," + c);
+            Console.WriteLine(a + "," + b + "," + c);
             if (c > b)
             {
-                b = c;  
+                b = z;
+                c = x;
             }
-            //Console.WriteLine(a + "," + b + "," + c);
+            Console.WriteLine(a + "," + b + "," + c);
             if (b > a)
             {
-                a = b;   
+                a = z;
+                b = y;
             }
-            //Console.WriteLine(a + "," + b + "," + c);
+            Console.WriteLine(a + "," + b + "," + c);
             Console.WriteLine("The biggest number is " + a);
 
             Console.ReadLine();
