f6e6b37ac2ebfb7a91dfe10eeef485fb1649e928
bhnamn <borhan.amini@gmail.com>
2016-01-14 23:51:13 +0200

    Initial version

---
+++ Demo1_Teh6/Demo1_Teh6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_Teh6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dist, x, cons, cost;
+
+            Console.WriteLine("Enter distance: ");
+            dist = double.Parse(Console.ReadLine());
+            x = dist / 100;
+            cons = x * 7.02;
+            cost = cons * 1.595;
+
+            Console.WriteLine("The Gasoline consumption is: " + cons + " The charge is: " + cost);
+            Console.ReadLine();
+        }
+    }
+}
