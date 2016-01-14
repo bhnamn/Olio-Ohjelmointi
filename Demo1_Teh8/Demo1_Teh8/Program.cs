f1ec4b53fa93370a5b10ead227a5bf81487987e7
bhnamn <borhan.amini@gmail.com>
2016-01-14 23:29:05 +0200

    Demo1_Teh8_1st solution

---
+++ Demo1_Teh8/Demo1_Teh8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_Teh8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, x, y, z;
+            Console.WriteLine("Please enter 3 integer numbers: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+           
+            if (b > a)
+            {
+                a = b; 
+            }
+            //Console.WriteLine(a + "," + b + "," + c);
+            if (c > b)
+            {
+                b = c;  
+            }
+            //Console.WriteLine(a + "," + b + "," + c);
+            if (b > a)
+            {
+                a = b;   
+            }
+            //Console.WriteLine(a + "," + b + "," + c);
+            Console.WriteLine("The biggest number is " + a);
+
+            Console.ReadLine();
+        }
+    }
+}
