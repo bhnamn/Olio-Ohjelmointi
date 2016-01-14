03be80296e21f3c6384810ad2b6366ab8e68786e
bhnamn <borhan.amini@gmail.com>
2016-01-14 23:46:19 +0200

    Demo1_Teh7

---
+++ Demo1_Teh7/Demo1_Teh7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_Teh7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Which year? > ");
+            year = int.Parse(Console.ReadLine());
+
+            if ((year % 4 == 0) && (year % 100 != 0))
+                Console.WriteLine("The year " + year + " is a leap year");
+            else if ((year % 100 == 0) && (year % 400 == 0))
+                Console.WriteLine("The year " + year + " is a leap year");
+            else
+                Console.WriteLine("The year " + year + " is not a leap year");
+
+            Console.ReadLine();
+        }
+    }
+}
