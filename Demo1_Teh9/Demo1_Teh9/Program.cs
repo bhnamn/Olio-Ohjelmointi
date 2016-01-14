d89f64ed2a95c5d6d97eec17ddebffd54067bf8f
bhnamn <borhan.amini@gmail.com>
2016-01-15 00:11:42 +0200

    Initial version

---
+++ Demo1_Teh9/Demo1_Teh9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_Teh9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int sum = 0;
+            do
+            {
+                Console.Write("Enter a number: ");
+                x = int.Parse(Console.ReadLine());
+                sum = sum + x;
+            } while (x != 0);
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Sum= " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
