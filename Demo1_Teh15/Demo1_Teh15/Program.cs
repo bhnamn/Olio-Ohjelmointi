9dd63de89812c80f8e77fb6d0b1aec6d1a250e27
bhnamn <borhan.amini@gmail.com>
2016-01-19 01:04:46 +0200

    initial

---
+++ Demo1_Teh15/Demo1_Teh15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1_Teh15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, row;
+            Console.Write("Enter row number: ");
+            row = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= row; i++)
+            {
+                for (j = 0; j < i; j++)
+                    Console.Write("*");
+                    Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
