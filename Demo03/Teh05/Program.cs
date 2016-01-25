766455e86f690b844c915eb1d266dd88138e3f8c
bhnamn <borhan.amini@gmail.com>
2016-01-25 14:20:48 +0200

    Authentication programme

---
+++ Demo03/Teh05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teh05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string enteredStudentID;
+            string enteredPassword;
+
+            Console.WriteLine("Enter Student ID: ");
+            enteredStudentID = Console.ReadLine();
+
+            Console.WriteLine("Enter Password: ");
+            enteredPassword = Console.ReadLine();
+
+            Student oStudent = new Student();
+            Boolean returnedValue = oStudent.Login(enteredStudentID, enteredPassword);
+            if (returnedValue == true)
+            {
+                Console.WriteLine("welcome " + oStudent.FirstName + " " + oStudent.LastName);
+                Console.WriteLine("Your group code is TTV15S" + oStudent.Group);
+            }
+            else
+            {
+                Console.WriteLine("Login Failed.");
+                Console.WriteLine("The userID & password are not matched.");
+            }
+        }
+    }
+}
