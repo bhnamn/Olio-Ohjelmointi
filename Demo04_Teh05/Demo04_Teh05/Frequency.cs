e6d3780c5f0e799c75bfbfea9e0842ae04a83028
bhnamn <borhan.amini@gmail.com>
2016-01-27 04:09:48 +0200

    initial not competed

---
+++ Demo04_Teh05/Demo04_Teh05/Frequency.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo04_Teh05
+{
+    class Frequency
+    {
+        private readonly double frequency (>=2000.0 || <=26000.0);
+
+        private int speed;
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                if (value <= MaxSpeed) speed = value;
+                else
+                {
+                    Console.WriteLine("Too much speed - set to maximum!");
+                    speed = MaxSpeed;
+                }
+            }
+        }
+    }
+}
