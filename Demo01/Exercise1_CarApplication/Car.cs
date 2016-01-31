ed246ed88d1394a63301984adf21bb4b7722c254
bhnamn <borhan.amini@gmail.com>
2016-01-31 19:18:26 +0200

    Car App, Lesson 1, Demo 1

---
+++ Demo01/Exercise1_CarApplication/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_CarApplication
+{
+    class Car
+    {
+        // Fields
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        private int speed;
+        public bool Turbo { get; set; }
+        public int DoorCount { get; set; }
+
+        // Property(making limit for speed value)
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value >= 0 && value <= 200) { speed = value; }
+                else speed = 0;
+            }
+        }
+        
+        // Method to speed up
+        public void Accelerator()
+        {
+            Speed += 5;
+        }
+
+        // Method to slow down
+        public void Brake()
+        {
+            Speed -= 5;
+        }
+
+        // Method to display car datas
+        public void PrintData()
+        {
+            Console.WriteLine("Car Data: ");
+            Console.WriteLine("- model : "+ Model);
+            Console.WriteLine("- color : " + Color);
+            Console.WriteLine("- engine : " + Engine+" Litre");
+            Console.WriteLine("- speed : " + Speed + " Km/h");
+            Console.WriteLine("- turbo : " + Turbo);
+            Console.WriteLine("- door count : " + DoorCount);
+        }
+    }
+}
---
+++ Demo01/Exercise1_CarApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Exercise1_CarApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise1_CarApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("83476ff6-c9da-4651-9b58-75abb64907a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
